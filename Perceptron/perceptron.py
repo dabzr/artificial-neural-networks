dbb425f7b84a7ab714699a8ca2154714a98dc09c
dabzr <dylanab@pm.me>
2025-08-22 13:33:27 -0300

    Organizando o perceptron em uma classe

--- Perceptron/perceptron.py
+++ Perceptron/perceptron.py
@@ -1,41 +1,50 @@
 import random
+
 def dotproduct(x, w):
     sum = 0
     for i, j in zip(x, w):
         sum += i*j
     return sum
 
-def update_weights(w, x, y):
-    for i in range(len(w)):
-        w[i] = w[i] + x[i]*y
-    return w
+class Perceptron:
+    def __init__(self, dataset, weights=None) -> None:
+        self.dataset = dataset
+        self.model = weights
 
-def get_x_aug(x, fixed_input=1):
-    return [fixed_input] + x
+    @staticmethod
+    def update_weights(w, x, y):
+        for i in range(len(w)):
+            w[i] = w[i] + x[i]*y
+        return w
 
-def training(dimension, dataset, max_iter, bias):
-    w = [bias] + [0]*dimension
-    for _ in range(max_iter):        
-        i = random.randint(0, len(dataset)-1)
-        x, y = dataset[i]
-        x_aug = get_x_aug(x)
-        xw = dotproduct(x_aug, w)
-        if (xw >= 0 and y == -1) or (xw < 0 and y == 1):
-            w = update_weights(w, x_aug, y)
-    return w
+    @staticmethod
+    def get_x_aug(x, fixed_input=1):
+        return [fixed_input] + x
 
-def result(x, w):
-    x_aug = get_x_aug(x)
-    if dotproduct(x_aug, w) >= 0:
-        return 1
-    return -1
+    def train(self, dimension, epochs, bias):
+        w = [bias] + [0]*dimension
+        for _ in range(epochs):        
+            i = random.randint(0, len(self.dataset)-1)
+            x, y = self.dataset[i]
+            x_aug = Perceptron.get_x_aug(x)
+            xw = dotproduct(x_aug, w)
+            if (xw >= 0 and y == -1) or (xw < 0 and y == 1):
+                w = Perceptron.update_weights(w, x_aug, y)
+        self.model = w
+    
+    def predict(self, x):
+        x_aug = Perceptron.get_x_aug(x)
+        if dotproduct(x_aug, self.model) >= 0:
+            return 1
+        return -1
 
 def test(dataset):
-    w = training(2, dataset, 50, 0)
-    print(f"[1, 1]={result([1, 1], w)}")
-    print(f"[1, 0]={result([1, 0], w)}")
-    print(f"[0, 1]={result([0, 1], w)}")
-    print(f"[0, 0]={result([0, 0], w)}")
+    p = Perceptron(dataset)
+    p.train(dimension=2, epochs=50, bias=0)
+    print(f"[1, 1]={p.predict([1, 1])}")
+    print(f"[1, 0]={p.predict([1, 0])}")
+    print(f"[0, 1]={p.predict([0, 1])}")
+    print(f"[0, 0]={p.predict([0, 0])}")
 
 or_dataset = [([1, 1], 1), ([1, 0], 1), ([0, 0], -1), ([0, 1], 1)]
 xor_dataset = [([1, 1], -1), ([1, 0], 1), ([0, 0], -1), ([0, 1], 1)]
